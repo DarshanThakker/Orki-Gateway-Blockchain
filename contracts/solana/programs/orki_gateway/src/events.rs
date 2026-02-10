8a76c0c3f868614068eb8f8465aa3c0d0ba0960f
SULEMAN ISMAILA <maxwellasekhome@gmail.com>
2026-02-10 22:44:20 +0100

    fix/updates

--- contracts/solana/programs/orki_gateway/src/events.rs
+++ contracts/solana/programs/orki_gateway/src/events.rs
@@ -1,16 +1,3 @@
-// use anchor_lang::prelude::*;
-
-// #[event]
-// pub struct PaymentProcessed {
-//     pub payer: Pubkey,
-//     pub merchant: Pubkey,
-//     pub amount: u64,
-//     pub fee: u64,
-//     pub token: Pubkey,
-//     pub payment_id: u64,
-//     pub timestamp: i64,
-// }
-
 
 
 use anchor_lang::prelude::*;
